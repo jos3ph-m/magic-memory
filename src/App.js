57562fee163c256b43e7ac15ac07b0874531a6a5
Joseph M <jmedina1985@hotmail.com>
2022-01-06 15:52:13 -0800

    shuffle cards

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
       .sort(() => Math.random() - 0.5)
       .map((card) => ({ ...card, id: Math.random() }));
 
-    setCards(shuffleCards);
+    setCards(shuffledCards);
   };
 
   return (
