bcda9b880c1506fc087b451e9debbfa17e73527b
Joseph M <jmedina1985@hotmail.com>
2022-01-08 15:18:35 -0800

    Making card choices

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -6,7 +6,12 @@ export const SingleCard = ({ card }) => {
     <div className="card">
       <div>
         <img className="front" src={card.src} alt="card front" />
-        <img className="back" src="/img/cover.png" onClick={} alt="card back" />
+        <img
+          className="back"
+          src="/img/cover.png"
+          onClick={handleClick}
+          alt="card back"
+        />
       </div>
     </div>
   );
