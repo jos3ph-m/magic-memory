60f385993ed7cf49bcafcb01b8f5ce762c049698
Joseph M <jmedina1985@hotmail.com>
2022-01-07 17:07:50 -0800

    creating grid of cards

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
           <div className="card" key={card.id}>
             <div>
               <img className="front" src={card.src} alt="card front" />
-              <img className="back" src="/img/cover.png" alt="" />
+              <img className="back" src="/img/cover.png" alt="card back" />
             </div>
           </div>
         ))}
