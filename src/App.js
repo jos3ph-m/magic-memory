19a724a62ad1624a0d1174d60a069934380096ba
Joseph M <jmedina1985@hotmail.com>
2022-01-10 15:19:00 -0800

    Add matched property

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const cardImages = [
   { src: '/img/helmet-1.png', matched: false },
   { src: '/img/potion-1.png', matched: false },
   { src: '/img/ring-1.png', matched: false },
-  { src: '/img/scroll-1.png' },
+  { src: '/img/scroll-1.png', matched: false },
   { src: '/img/shield-1.png' },
   { src: '/img/sword-1.png' },
 ];
