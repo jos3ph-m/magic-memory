eb39fcab4506338d371340a944adbcf5e0add89d
Joseph M <jmedina1985@hotmail.com>
2022-01-10 15:18:51 -0800

    Add matched property

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { SingleCard } from './components/SingleCard';
 const cardImages = [
   { src: '/img/helmet-1.png', matched: false },
   { src: '/img/potion-1.png', matched: false },
-  { src: '/img/ring-1.png' },
+  { src: '/img/ring-1.png', matched: false },
   { src: '/img/scroll-1.png' },
   { src: '/img/shield-1.png' },
   { src: '/img/sword-1.png' },
