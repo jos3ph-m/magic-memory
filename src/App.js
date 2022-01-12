81892a7bd6963d7a65cd9219f9cbcb57caaecdcf
Joseph M <jmedina1985@hotmail.com>
2022-01-11 16:35:54 -0800

    Flipping cards

--- src/App.js
+++ src/App.js
@@ -74,7 +74,7 @@ function App() {
             key={card.id}
             card={card}
             handleChoice={handleChoice}
-            flipped={card === choiceOne || card === choiceTwo}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
           />
         ))}
       </div>
