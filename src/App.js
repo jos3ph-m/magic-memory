7c8a2c53d9989d357d63824c445e18ae3735ffb0
Joseph M <jmedina1985@hotmail.com>
2022-01-11 16:35:00 -0800

    Flipping cards

--- src/App.js
+++ src/App.js
@@ -70,7 +70,12 @@ function App() {
       <button onClick={shuffleCards}>New Game</button>
       <div className="card-grid">
         {cards.map((card) => (
-          <SingleCard key={card.id} card={card} handleChoice={handleChoice} flipped={}/>
+          <SingleCard
+            key={card.id}
+            card={card}
+            handleChoice={handleChoice}
+            flipped={card === choiceOne}
+          />
         ))}
       </div>
     </div>
