2463feeffd781eb3eaa30f70bc77f004083693d4
Joseph M <jmedina1985@hotmail.com>
2022-01-13 18:36:30 -0800

    Disabled prop

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './SingleCard.css';
 
-export const SingleCard = ({ card, handleChoice, flipped }) => {
+export const SingleCard = ({ card, handleChoice, flipped, disabled }) => {
   const handleClick = () => {
     handleChoice(card);
   };
